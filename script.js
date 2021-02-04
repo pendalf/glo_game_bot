5f494e0d13a34ca16f7bc5c6d810237c60b4eb19
Zander <akma_2003@mail.ru>
2021-02-04 21:03:38 +0300

    Выполнение урока 6 по ТЗ: Используйте функции alert, confirm, prompt для общения с пользователем.
    Написать игровой бот.
    
    "Загадывание случайного числа от 1 до 100"
    
    Что должна делать программа:
    
    — спрашивает пользователя: "Угадай число от 1 до 100".
    — если пользовательское число больше, то бот выводит "Загаданное число меньше" и предлагает ввести новый вариант;
    — если пользовательское число меньше, то бот выводит "Загаданное число больше" и предлагает ввести новый вариант;
    — если пользователь ввел не число, то выводит сообщение "Введи число!" и предлагает ввести новый вариант;
    — если пользователь нажимает "Отмена", то игра заканчивается и выводится сообщение "Игра окончена".
    
    —  если пользовательское число равно загаданному, то игра заканчивается и выводит сообщение  "Поздравляю, Вы угадали!!!".
    
    Программа должны быть выполнена с помощью рекурсии, без единого цикла.
    
    Загаданное число должно храниться «в замыкании»

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
 'use strict';
+
+// Функция проверки переменной на число
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+// Функция для получения случайного чилса. Максимум и минимум включаются
+const getRandomInt = function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// Функция для опроса пользователя при помощи prompt
+const numPromt = (correct, incorrect, repeat = false, defValue = '') => {
+    const promtText = repeat ? incorrect : correct;
+    let num = prompt(promtText, defValue);
+    if (num === null) {
+        return num;
+    }
+    if (!isNumber(num)) {
+        num = numPromt(correct, incorrect, true, defValue);
+    }
+    return +num;
+};
+
+const gameStart = function() {
+
+    const min = 1; // Минимальное число для гнерации случайного числа
+    const max = 100; // Максимальное число для гнерации случайного числа
+    const correct = `Угадайте число от ${min} до ${max}`;
+    const incorrect = `Введите число от ${min} до ${max}`;
+
+    // Загадывание случайного числа от 1 до 100
+    const numRandom = getRandomInt(min, max);
+
+    // Функция бота
+    const gameBot = function(correct, incorrect) {
+
+        let numAns = numPromt(correct, incorrect);
+
+        if (numAns === null) {
+            alert('Игра окончена');
+            return;
+        } else if (numAns < min || numAns > max) {
+            gameBot(correct, incorrect);
+        } else if (numAns > numRandom) {
+            correct = `Загаданное число меньше. Угадайте число от ${min} до ${max}`;
+            gameBot(correct, incorrect);
+        } else if (numAns < numRandom) {
+            correct = `Загаданное число больше. Угадайте число от ${min} до ${max}`;
+            gameBot(correct, incorrect);
+        } else {
+            alert('Поздравляю, Вы угадали!!!');
+        }
+    };
+
+    gameBot(correct, incorrect);
+};
+
+gameStart();
